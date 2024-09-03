abbf2985892b17ca1af78a2f683a5eacdec97181
Bericutesy <callejaerick2000@gmail.com>
2024-09-03 23:26:27 +0800

    resolving conflicts
    build.gradle.kts
    androidmanifest.xml

--- FertiliSense/app/build.gradle.kts
+++ FertiliSense/app/build.gradle.kts
@@ -50,4 +50,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("com.prolificinteractive:material-calendarview:1.4.3")
 
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
 }
