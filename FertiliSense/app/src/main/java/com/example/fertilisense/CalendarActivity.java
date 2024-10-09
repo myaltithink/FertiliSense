bdcb2ed9aa8095cb67b25295599865f62fe3b4ce
Bericutesy <callejaerick2000@gmail.com>
2024-10-09 18:58:17 +0800

    Reportactivity
    colors
    activity_report
    ic_download
    ReportActivity
    FertiliSenseDashboardActivity
    FemaleReproductiveSystemActivity
    CalendarActivity
    AndroidManifest

--- FertiliSense/app/src/main/java/com/example/fertilisense/CalendarActivity.java
+++ FertiliSense/app/src/main/java/com/example/fertilisense/CalendarActivity.java
@@ -108,10 +108,10 @@ public class CalendarActivity extends AppCompatActivity implements NavigationVie
                     finish();
                 } else if (id == R.id.report) {
                     Log.d("FertiliSense", "User report details clicked");
-//                    Intent intent = new Intent(ReportActivity.this, ChatBotActivity.class);
-//                    startActivity(intent);
-//                    overridePendingTransition(0, 0);
-//                    finish();
+                    Intent intent = new Intent(CalendarActivity.this, ReportActivity.class);
+                   startActivity(intent);
+                    overridePendingTransition(0, 0);
+                    finish();
                 }
 
                 return true;
